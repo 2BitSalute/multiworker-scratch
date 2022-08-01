026188b4f4703b8f60089bb547adf814a990d8af
2BitSalute <2BitSalute@gmail.com>
2022-08-01 10:34:39 -0400

    Continuous index reading

--- src/demo_bz2.ml
+++ src/demo_bz2.ml
@@ -13,15 +13,16 @@
 
 open Printf
 
-let run iname bytes_to_skip =
+let catalog iname bytes_to_skip =
   let ic = open_in iname in
 
   In_channel.seek ic bytes_to_skip;
 
-  let oc =
+  let (oc, filename) =
     if not (Filename.check_suffix iname ".bz2") then
       eprintf "Error: unrecognized compressed file extension" ;
-    open_out (Filename.chop_suffix iname ".bz2")
+    let filename = Filename.chop_suffix iname ".bz2" in
+    open_out (filename), filename
   in
 
   let buflen =
@@ -32,6 +33,9 @@ let run iname bytes_to_skip =
   let buf = Bytes.create buflen in
 
   let bzic = Bz2.open_in ic in
+
+  (* TODO: Factor this out! *)
+  Out_channel.output_string oc "<pages>\n";
 
   let rec read n =
     if n <> 0 then begin
@@ -49,6 +53,81 @@ let run iname bytes_to_skip =
 
   read (-1);
 
+  (* TODO: Factor this out! *)
+  Out_channel.output_string oc "</pages>\n";
+
   Bz2.close_in bzic;
   close_in ic;
-  close_out oc
+  close_out oc;
+
+  filename
+
+type entry = {
+  offset: int;
+  id: int;
+  title: string;
+}
+
+let index iname =
+  let ic = open_in iname in
+
+  let buflen =
+    128
+    (* 256 *)
+  in
+
+  let buf = Bytes.create buflen in
+
+  let bzic = Bz2.open_in ic in
+
+  let get_entry s =
+    let tokens = Pcre.split ~pat:":" ~max:0 s in
+    List.iter (fun s -> Printf.printf "- %s\n" s) tokens;
+    {
+      offset = int_of_string (List.nth tokens 0);
+      id = int_of_string (List.nth tokens 1);
+      title = List.nth tokens 2;
+    }
+  in
+
+  let rec read n prefix =
+    if n <> 0 then begin
+      try
+        let bytes_read = Bz2.read bzic buf 0 buflen in
+        let start = ref 0 in
+        let length = ref 0 in
+        let prefix = ref prefix in
+        let f c =
+          if c = '\n' then begin
+            let entry = get_entry (!prefix ^ Bytes.sub_string buf !start !length) in
+            Printf.printf "%d %d %s\n" entry.offset entry.id entry.title;
+            start := !start + !length + 1;
+            length := 0;
+            prefix := "";
+          end
+          else
+            length := !length + 1;
+        in
+        Bytes.iter f buf;
+        let prefix =
+          if !length > 0 then
+            Bytes.sub_string buf !start !length
+          else
+            ""
+        in
+
+        (* Out_channel.output Out_channel.stdout buf 0 bytes_read; *)
+        if bytes_read < buflen then
+          raise End_of_file
+        else
+          read (n - 1) prefix
+      with End_of_file ->
+        ()
+    end
+  in
+
+  (* read (-1); *)
+  read 3 "";
+
+  Bz2.close_in bzic;
+  close_in ic
---
+++ src/demo_xmlm.ml
@@ -0,0 +1,44 @@
+let to_topic substrings =
+  let s = Pcre.get_substring !substrings 1 in
+  let strings = Pcre.split ~pat:"\\|" ~max:0 s in
+  Pcre.replace ~templ:"_" (List.hd strings)
+
+let examine_text i =
+  let () = match Xmlm.input i with
+    | `Data data ->
+      begin
+        let rex = Pcre.regexp {|\[\[([^[{]+)\]\]|} in
+        try
+          let (substrings: Pcre.substrings ref) = ref (Pcre.exec ~rex data) in
+          while true do
+            let topic = to_topic substrings in
+            Printf.printf "%s\n" topic;
+            substrings := Pcre.next_match ~rex !substrings;
+          done;
+        with Not_found ->
+          Printf.printf "No links matched!\n%!"
+      end
+    | _ -> assert false
+  in
+  ()
+
+let find_links ic =
+  let i = Xmlm.make_input (`Channel ic) in
+  let rec process i depth =
+    match Xmlm.input i with
+    | `El_start ((_namespace, tag), _attributes) when tag = "text" ->
+      examine_text i;
+      process i (depth + 1)
+    | `El_start _ -> process i (depth + 1)
+    | `El_end -> if depth = 0 then () else process i (depth - 1)
+    | `Data _ -> process i depth
+    | `Dtd _ -> assert false
+  in
+  let _dtd = Xmlm.input i in
+  let _root = Xmlm.input i in
+  process i 0
+
+let run filename =
+  let ic = In_channel.open_bin filename in
+  find_links ic;
+  ()

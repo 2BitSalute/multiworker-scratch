9574001ab361d9d6d8932cee1e5a7b8311cb65bf
2BitSalute <2BitSalute@gmail.com>
2022-08-03 09:24:38 -0400

    Part 2

--- src/demo_xmlm.ml
+++ src/demo_xmlm.ml
@@ -3,30 +3,31 @@ let to_topic substrings =
   let strings = Pcre.split ~pat:"\\|" ~max:0 s in
   List.hd strings
 
-let examine_text i =
-  let () = match Xmlm.input i with
-    | `Data data ->
-      begin
-        let rex = Pcre.regexp {|\[\[([^[{]+)\]\]|} in
-        try
-          let (substrings: Pcre.substrings ref) = ref (Pcre.exec ~rex data) in
-          while true do
-            let topic = to_topic !substrings in
-            Printf.printf "%s\n" topic;
-            substrings := Pcre.next_match ~rex !substrings;
-          done;
-        with Not_found ->
-          Printf.printf "No links matched!\n%!"
-      end
-    | _ -> assert false
-  in
-  ()
+let examine_text i : string list=
+  let topics = ref [] in
+  match Xmlm.input i with
+  | `Data data ->
+    begin
+      let rex = Pcre.regexp {|\[\[([^[{]+)\]\]|} in
+      try
+        let (substrings: Pcre.substrings ref) = ref (Pcre.exec ~rex data) in
+        while true do
+          let topic = to_topic !substrings in
+          topics := topic :: !topics;
+          Printf.printf "%s\n" topic;
+          substrings := Pcre.next_match ~rex !substrings;
+        done;
+      with Not_found ->
+        Printf.printf "No links matched!\n%!";
+    end;
+    !topics
+  | _ -> assert false
 
 let find_links i =
   let rec process i depth =
     match Xmlm.input i with
     | `El_start ((_namespace, tag), _attributes) when tag = "text" ->
-      examine_text i;
+      let _topics = examine_text i in
       process i (depth + 1)
     | `El_start _ -> process i (depth + 1)
     | `El_end -> if depth = 0 then () else process i (depth - 1)
